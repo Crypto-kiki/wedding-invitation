7f71a7fbb05504f36558347460f48bdf9933f2a1
Crypto-kiki <ejo2123@naver.com>
2025-06-22 16:59:17 +0800

    gitignore

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__72c354d2._.css",
-  "static/chunks/node_modules_next_dist_05476590._.js",
-  "static/chunks/src_contexts_LanguageContext_tsx_ac0618fd._.js"
-],
-    source: "dynamic"
-});
